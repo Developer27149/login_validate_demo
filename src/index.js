a7d58dff5903be0c7b022f5e7364935878f89925
youyiqin <564151947@qq.com>
2021-06-01 01:34:13 +0800

    add something

--- src/index.js
+++ src/index.js
@@ -3,20 +3,27 @@ import React, { useState } from "react";
 import { render } from "react-dom";
 
 function App() {
-    const [state, setState] = useState("CLICK ME");
+    const [state, setState] = useState("something error");
+    const sub = (e) => {
+      e.preventDefault();
 
+      console.log('submit');
+    }
     return (
       <>
       <header>
         <nav className="login">SIGN IN</nav>
         <nav>SIGN UP</nav>
       </header>
-      <form>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
+      <form onSubmit={sub}>
+        <input type="text" maxLength="12" placeholder="username"/>
+        <input type="password" minLength="6" pattern="^\d.*$" placeholder="password"/>
         <input type="email" placeholder="your email"/>
         <button>Login</button>
       </form>
+      {
+        state && <section>{state} 🤔</section>
+      }
       <footer>
         <a href="#">Forget your password?</a>
       </footer>
